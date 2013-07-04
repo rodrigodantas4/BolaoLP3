a6daed7eba26dd9d5007da53ef31d5cb0dd760fd
Ramon Celestino Ramos <ramoncelesteramos@gmail.com>
2013-07-04 16:47:25 -0300

    Inicio projeto

---
+++ BolaoLp3/src/com/example/bolaolp3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.bolaolp3;
+
+import java.util.Random;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+   
+    
+    public void mostrarTexto( View button){
+    	startActivity(new Intent(this, Cadastro.class));      }
+    
+    
+    
+}
