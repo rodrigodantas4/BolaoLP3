b2fdbfe7706bcf39882e332bb49c84d10ea03af4
Ramon Celestino Ramos <ramoncelesteramos@gmail.com>
2013-07-04 23:36:27 -0300

    Adicionadas as telas de ajuda e tabela

--- BolaoLp3/src/com/example/bolaolp3/Ajuda.java
+++ BolaoLp3/src/com/example/bolaolp3/Ajuda.java
@@ -2,27 +2,21 @@ package com.example.bolaolp3;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class MenuInicial extends Activity {
+public class Ajuda extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu_inicial);
+		setContentView(R.layout.activity_ajuda);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_inicial, menu);
+		getMenuInflater().inflate(R.menu.ajuda, menu);
 		return true;
 	}
-	
-	public void entrarTabela( View button){
-    	startActivity(new Intent(this, Tabela.class));      }
-	
 
 }
--- BolaoLp3/src/com/example/bolaolp3/Times.java
+++ BolaoLp3/src/com/example/bolaolp3/Times.java
@@ -2,27 +2,21 @@ package com.example.bolaolp3;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class MenuInicial extends Activity {
+public class Times extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_menu_inicial);
+		setContentView(R.layout.activity_times);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_inicial, menu);
+		getMenuInflater().inflate(R.menu.times, menu);
 		return true;
 	}
-	
-	public void entrarTabela( View button){
-    	startActivity(new Intent(this, Tabela.class));      }
-	
 
 }
