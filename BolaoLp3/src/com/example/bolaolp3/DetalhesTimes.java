4d74a9d293094f4f9af2a1a56f427d7d94c8a5d6
Ramon Celestino Ramos <ramoncelesteramos@gmail.com>
2013-07-06 00:15:23 -0300

    Activity detalhes times

--- BolaoLp3/src/com/example/bolaolp3/DetalhesTimes.java
+++ BolaoLp3/src/com/example/bolaolp3/DetalhesTimes.java
@@ -2,27 +2,21 @@ package com.example.bolaolp3;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class Times extends Activity {
+public class DetalhesTimes extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_times);
+		setContentView(R.layout.activity_detalhes_times);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.times, menu);
+		getMenuInflater().inflate(R.menu.detalhes_times, menu);
 		return true;
 	}
 
-	public void entrarDetalhes(View button){
-		startActivity(new Intent(this, DetalhesTimes.class));
-	}
-	
 }
